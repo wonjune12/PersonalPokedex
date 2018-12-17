1bc4b5627e56c886b0f7bc4df02e19522bafdc9c
Wonjun Choi <wonjune12@hotmail.com>
2018-12-17 12:46:55 -0500

    attempt to change css background

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 "use strict";
 
 class Pokemon{
-    constructor(name,attackPower,healthPoint,abilities){
+    constructor(name,attackPower,healthPoint,abilities,type){
         this.name = name;
         this.attackPower = attackPower;
         this.healthPoint = healthPoint;
         this.abilities = abilities;
+        this.type = type;
     }
 }
 
@@ -19,6 +20,9 @@ axios.get("https://pokeapi.co/api/v2/pokemon/387/")
 .then(function (response){
 
     let pokeData = response.data;
+    if (pokeData.types[0].slot=="1"){
+        var pokeType = pokeData.types[0].type.name;
+    }
     var arrAbs = [];    
     for (let i = 0; i < response.data.abilities.length; i++){
         arrAbs.push(response.data.abilities[i].ability.name)
@@ -27,14 +31,18 @@ axios.get("https://pokeapi.co/api/v2/pokemon/387/")
     pokeData.name,
     pokeData["stats"][4].base_stat,
     pokeData["stats"][5].base_stat,
-    arrAbs
+    arrAbs,
+    pokeType
+
     )
     let pokemon11 = document.getElementById("pokemon1");
     pokemon11.innerText = ` ${tName}'s Pokemon
                             Name : ${capitalizeFirstLetter(pokemon1.name)}
                             Attack : ${pokemon1.attackPower} 
                             HP  : ${pokemon1.healthPoint} 
-                            Abilities : ${pokemon1.abilities}`;
+                            Abilities : ${pokemon1.abilities}
+                            Type : ${pokemon1.type}`;
+    localStorage.setItem("types",pokeType);
 })
 .catch(function(error){
     console.log(error);
@@ -45,6 +53,9 @@ axios.get("https://pokeapi.co/api/v2/pokemon/390/")
 
     let pokeData = response.data;
     var arrAbs = [];    
+    if (pokeData.types[0].slot=="1"){
+        var pokeType = pokeData.types[0].type.name;
+    }
     for (let i = 0; i < pokeData.abilities.length; i++){
         arrAbs.push( pokeData.abilities[i].ability.name)
     }
@@ -53,14 +64,17 @@ axios.get("https://pokeapi.co/api/v2/pokemon/390/")
     pokeData.name,
     pokeData["stats"][4].base_stat,
     pokeData["stats"][5].base_stat,
-    arrAbs
+    arrAbs,
+    pokeType
     )
     let pokemon22 = document.getElementById("pokemon2");
     pokemon22.innerText = ` ${tName}'s Pokemon
                             Name : ${capitalizeFirstLetter(pokemon2.name)}
                             Attack : ${pokemon2.attackPower}
                             HP  : ${pokemon2.healthPoint}
-                            Abilities : ${pokemon2.abilities}`;
+                            Abilities : ${pokemon2.abilities}
+                            Type : ${pokemon2.type}`;
+    localStorage.setItem("types",pokeType);
 })
 .catch(function(error){
     console.log(error);
@@ -74,19 +88,24 @@ axios.get("https://pokeapi.co/api/v2/pokemon/393/")
     for (let i = 0; i < response.data.abilities.length; i++){
         arrAbs.push(response.data.abilities[i].ability.name)
     }
-
+    if (pokeData.types[0].slot=="1"){
+        var pokeType = pokeData.types[0].type.name;
+    }
     let pokemon3 = new Pokemon(
     pokeData.name,
     pokeData["stats"][4].base_stat,
     pokeData["stats"][5].base_stat,
-    arrAbs
+    arrAbs,
+    pokeType
     )
     let pokemon33 = document.getElementById("pokemon3");
     pokemon33.innerText = ` ${tName}'s Pokemon
                             Name : ${capitalizeFirstLetter(pokemon3.name)}
                             Attack : ${pokemon3.attackPower}
                             HP  : ${pokemon3.healthPoint}
-                            Abilities : ${pokemon3.abilities}`;
+                            Abilities : ${pokemon3.abilities}
+                            Type : ${pokemon3.type}`;
+    localStorage.setItem("types",pokeType);
 
 })
 .catch(function(error){
