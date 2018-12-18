4d1863690d5a3717b44196467b7b57ce10ec58b4
Wonjun Choi <wonjune12@hotmail.com>
2018-12-17 20:10:06 -0500

    again. fixing

--- main1.js
+++ main1.js
@@ -1,21 +1,22 @@
 "use strict";
 
 var clickEvent = document.querySelector("button");
-var showRow = document.querySelector(".row");
-var typeBackground = document.getElementById("page*");
+var showMain = document.querySelector("#main");
+// var typeBackground = document.getElementById("page*");
 
 
 clickEvent.addEventListener("click",(event)=>{
     localStorage.setItem("key",document.getElementById("trainer-name").value); 
-    var getType = localStorage.getItem["types"];
-    if (getType == "grass"){
-        document.querySelector("#page1").classList.add("page1");
-    }
-    if (getType == "fire"){
-        document.querySelector("#page2").classList.add("page2");
-    }
-    if (getType == "water"){
-        document.querySelector("#page3").classList.add("page3");
-    }
+    // var getType = localStorage.getItem["types"];
+    // if (getType == "grass"){
+    //     document.querySelector("#page1").classList.add("page1");
+    // }
+    // if (getType == "fire"){
+    //     document.querySelector("#page2").classList.add("page2");
+    // }
+    // if (getType == "water"){
+    //     document.querySelector("#page3").classList.add("page3");
+    // }
+    showMain.classList.add("slideDown");
 })
 
