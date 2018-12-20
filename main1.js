1cb498d9497164e9f7324dfb30c5c14f771f8bb3
Wonjun Choi <wonjune12@hotmail.com>
2018-12-20 17:48:25 -0500

    made all css grid pages

--- main1.js
+++ main1.js
@@ -8,14 +8,6 @@ var typeBackground = document.getElementById("page*");
 clickEvent.addEventListener("click",(event)=>{
     localStorage.setItem("key",document.getElementById("trainer-name").value); 
     var getType = localStorage.getItem["types"];
-    if (getType == "grass"){
-        document.querySelector("#page1").classList.add("page1");
-    }
-    if (getType == "fire"){
-        document.querySelector("#page2").classList.add("page2");
-    }
-    if (getType == "water"){
-        document.querySelector("#page3").classList.add("page3");
-    }
+    showRow.classList.add("slideDown")
 })
 
